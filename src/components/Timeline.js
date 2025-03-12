d95063e68cdcae13ef3808d237edc0d6d595ed24
Ashish Patel <apatel2@ualberta.ca>
2025-03-12 00:13:16 -0600

    feat: add placeholder Timeline component

---
+++ src/components/Timeline.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Timeline = () => (
+  <View>
+    <Text>Timeline Component</Text>
+  </View>
+);
+
+export default Timeline;
