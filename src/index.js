596eda95e98a637f433763fc7ff5583b2f16805a
Ashish Patel <apatel2@ualberta.ca>
2025-03-12 00:12:04 -0600

    feat: add src/index.js to bootstrap the app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { AppRegistry } from "react-native";
+
+AppRegistry.registerComponent("App", () => App);
+AppRegistry.runApplication("App", {
+  initialProps: {},
+  rootTag: document.getElementById("root"),
+});
