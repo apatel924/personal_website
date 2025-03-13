bb953b3ac870c6daeb2ea1440fea76c87958ce36
Ashish Patel <apatel2@ualberta.ca>
2025-03-13 01:42:48 -0600

    wrap lay out and added styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
+import Layout from "./components/Layout";
 
-const App = () => (
-  <View style={styles.container}>
-    <Text style={styles.title}>Welcome to Ashish Patel's Personal Website</Text>
-    {/* Future components will be integrated here */}
-  </View>
-);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-});
+const App = () => {
+  return (
+    <Layout>
+      <View>
+        <Text style={{ color: "#64ffda", fontSize: 24 }}>
+          Welcome to my personal website!
+        </Text>
+      </View>
+    </Layout>
+  );
+};
 
 export default App;
